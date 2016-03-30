401578580590785b9432a908937e853b0683c82a
Balanar300hp <kudashov.vlad@yandex.ru>
2016-03-30 19:11:03 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -10,17 +10,17 @@ SCENARIO("Matrix init", "[init]") {
 		auto columns = 3;
 
 		WHEN("Create instansce of Matrix") {
-			matrix matrix(rows, columns);
+			Matrix Matrix(rows, columns);
 			THEN("The number of rows and columns must be preserved") {
 
-				REQUIRE(matrix.get_num_rows() == rows);
-				REQUIRE(matrix.get_num_cols() == columns);
+				REQUIRE(Matrix.get_num_rows() == rows);
+				REQUIRE(Matrix.get_num_cols() == columns);
 			}
 		}
 	}
 }
 SCENARIO("Matrix +", "[addition]") {
-	Matrix A = matrix(2, 2);
+	Matrix A = Matrix(2, 2);
 	A.get_from_file("A2x2.txt");
 	Matrix B = Matrix(2, 2);
 	B.get_from_file("B2x2.txt");
@@ -35,7 +35,7 @@ SCENARIO("Matrix *", "[addition]") {
 	A.get_from_file("A2x2.txt");
 	Matrix B = Matrix(2, 2);
 	B.get_from_file("B2x2.txt");
-Matrix expected = matrix(2, 2);
+Matrix expected = Matrix(2, 2);
 	expected.get_from_file("A*B2x2.txt");
 	Matrix result = A * B;
 	REQUIRE(result == expected);
