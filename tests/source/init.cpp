ca2c4e089287dc1f13af1ea9aa6841559b5153f0
Balanar300hp <kudashov.vlad@yandex.ru>
2016-03-30 19:17:42 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -13,38 +13,38 @@ SCENARIO("Matrix init", "[init]") {
 			Matrix Matrix(rows, columns);
 			THEN("The number of rows and columns must be preserved") {
 
-				REQUIRE(Matrix.get_num_rows() == rows);
-				REQUIRE(Matrix.get_num_cols() == columns);
+				REQUIRE(Matrix.get_rows() == rows);
+				REQUIRE(Matrix.get_columns() == columns);
 			}
 		}
 	}
 }
 SCENARIO("Matrix +", "[addition]") {
 	Matrix A = Matrix(2, 2);
-	A.get_from_file("A2x2.txt");
+	A.Get_Matrix("A2x2.txt");
 	Matrix B = Matrix(2, 2);
-	B.get_from_file("B2x2.txt");
+	B.Get_Matrix("B2x2.txt");
 	Matrix expected = Matrix(2, 2);
-	expected.get_from_file("A+B2x2.txt");
+	expected.Get_Matrix("A+B2x2.txt");
 	Matrix result = A + B;
 	REQUIRE(result == expected);
 }
 
 SCENARIO("Matrix *", "[addition]") {
 	Matrix A = Matrix(2, 2);
-	A.get_from_file("A2x2.txt");
+	A.Get_Matrix("A2x2.txt");
 	Matrix B = Matrix(2, 2);
-	B.get_from_file("B2x2.txt");
+	B.Get_Matrix("B2x2.txt");
 Matrix expected = Matrix(2, 2);
-	expected.get_from_file("A*B2x2.txt");
+	expected.Get_Matrix("A*B2x2.txt");
 	Matrix result = A * B;
 	REQUIRE(result == expected);
 }
 
 SCENARIO("Matrix: operator ==", "[equal]") {
 	Matrix A, B;
-	A.get_from_file("A2x2.txt");
-	B.get_from_file("A2x2.txt");
+	A.Get_Matrix("A2x2.txt");
+	B.Get_Matrix("A2x2.txt");
 	bool f = (A == B);
 	REQUIRE(f);
 }
