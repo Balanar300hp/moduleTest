006dddda82a65796270b801dccf486e6abc7572a
Balanar300hp <kudashov.vlad@yandex.ru>
2016-03-31 17:36:39 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -51,6 +51,7 @@ SCENARIO("Matrix: operator ==", "[equal]") {
 SCENARIO("Matrix: operator []", "[addition]") {
 	Matrix A = Matrix(2, 2);
 	A.Get_Matrix("A2x2.txt");
-	A[3].expected.get_num("A.txt");
+	int expected = A[3];
+	expected.get_num("A.txt");
 	REQUIRE(A[3]==expected);
 }
