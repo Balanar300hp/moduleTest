8ef28c236dfbf4b73d430462de68dabd267199b5
Balanar300hp <kudashov.vlad@yandex.ru>
2016-04-20 01:55:36 +0300

    Update matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -1,50 +1,48 @@
 #pragma once
 #ifndef MATR_H
 #define NATR_H
-
 #include "MatrixException.h"
 #include <iostream>
 #include <cstring> 
 
 template <typename T>
-class CMatrix;
+class Matrix;
 
 template <class T>
-std::ostream & operator<<(std::ostream & output, const CMatrix<T> &);
+std::ostream & operator<<(std::ostream & output, const Matrix<T> &);
 
 template <class T>
-std::istream & operator>>(std::istream & input, CMatrix<T> &);
+std::istream & operator>>(std::istream & input, Matrix<T> &);
 
 
 template <typename T>
-class CMatrix {
+class Matrix {
 private:
-	void swap(CMatrix & x);
+	void swap(Matrix & x);
 	T **matrix;
-	int m_columns;
-	int m_rows;
+	int columns;
+	int rows;
 
 public:
-	CMatrix() : m_rows(0), m_columns(0), matrix(nullptr) {};	// Done
-	CMatrix(unsigned int n, unsigned int m);					// Done
-	CMatrix(T **matr, unsigned int _n, unsigned int _m);
-	CMatrix(CMatrix const & x);
-	~CMatrix();
+	Matrix() : rows(0), columns(0), matrix(nullptr) {};	// Done
+	Matrix(unsigned int n, unsigned int m);					// Done
+	Matrix(T **matr, unsigned int _n, unsigned int _m);
+	Matrix(Matrix const & x);
+	~Matrix();
 
 	unsigned int rowsNumber() const;							// Done
 	unsigned int columnsNumber() const;							// Done
 	bool readFromFile(char *path);								// Done
-
-	CMatrix &operator =(const CMatrix& m2);
-	CMatrix operator +(const CMatrix& m2);						// Done
-	CMatrix operator -(const CMatrix& m2);						// Done
-	CMatrix operator *(double num);								// Done
-	CMatrix operator *(const CMatrix& m2);						// Done
-	bool operator ==(const CMatrix& m2);						// Done
+	Matrix &operator =(const Matrix& m2);
+	Matrix operator +(const Matrix& m2);						// Done
+	Matrix operator -(const Matrix& m2);						// Done
+	Matrix operator *(double num);								// Done
+	Matrix operator *(const Matrix& m2);						// Done
+	bool operator ==(const Matrix& m2);						// Done
 	T* operator [](unsigned int index);							// Done
 
-	friend std::ostream & operator<< <>(std::ostream & output, const CMatrix & matrix);	// Done
-	friend std::istream & operator>> <>(std::istream & input, CMatrix<T> & matrix);		// Done
+	friend std::ostream & operator<< <>(std::ostream & output, const Matrix & matrix);	// Done
+	friend std::istream & operator>> <>(std::istream & input, Matrix<T> & matrix);		// Done
 };
 
 
