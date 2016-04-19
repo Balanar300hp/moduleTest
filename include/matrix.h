c3f6e8b380ab428186f62c99e73173c67d3e9dd1
Balanar300hp <kudashov.vlad@yandex.ru>
2016-04-19 21:12:50 +0300

    Update matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -28,8 +28,8 @@ public:
 	T* operator [](int i);
 	int get_rows();
 	int get_columns();
-	friend std::ostream & operator<< <>(std::ostream & output, const CMatrix & matrix);	
-	friend std::istream & operator>> <>(std::istream & input, CMatrix<T> & matrix);	
+	friend std::ostream & operator<< <>(std::ostream & output, const Matrix & matrix);	
+	friend std::istream & operator>> <>(std::istream & input, Matrix<T> & matrix);	
 private:
 	void swap(Matrix & m1);
 	string s{};
