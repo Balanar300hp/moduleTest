50e51a7c5dba459c3eda604cd1cfcdc142464412
Balanar300hp <kudashov.vlad@yandex.ru>
2016-04-19 22:13:10 +0300

    Update matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -13,10 +13,10 @@ using namespace std;
 template <typename T>
 class Matrix;
  
-template <typename T>
+template <class T>
 std::ostream & operator<<(std::ostream & output, const Matrix<T> &);
 
-template <typename T>
+template <class T>
 std::istream & operator>>(std::istream & input, Matrix<T> &);
 
 template <typename T>
